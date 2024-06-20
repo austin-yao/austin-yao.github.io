88c0deecf11adad2078af730dad939ad7f1f8f38
austin-yao <austin.y.yao@gmail.com>
2024-06-20 00:37:49 -0400

    temp fix

--- src/components/writing.jsx
+++ src/components/writing.jsx
@@ -5,7 +5,7 @@ function Writing() {
         <div>
             <Header />
             <b><a href="https://mode.mirror.xyz/ZCSIB3AwHgUYcxMBDAX8s4KnYmrISiKbbrbfaxsMeM8">Cooperative Game Theory On-Chain: Part 1</a></b><br></br>
-            <b><a href="/entry/decentralization">The Myth of Decentralization</a></b><br />
+            <b><a href="https://open.substack.com/pub/austinyao/p/the-myth-of-decentralization?r=m0j4w&utm_campaign=post&utm_medium=web">The Myth of Decentralization</a></b><br />
             <b>...</b>
         </div>
     )
