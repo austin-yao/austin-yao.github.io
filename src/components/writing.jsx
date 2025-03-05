50c0d21f0084bd34978f5b86e0ba71442f6c789a
austin-yao <austin.y.yao@gmail.com>
2025-03-05 00:06:09 -0500

    update old writing

--- src/components/writing.jsx
+++ src/components/writing.jsx
@@ -4,7 +4,8 @@ function Writing() {
     return (
         <div>
             <Header />
-            <b><a href="https://mode.mirror.xyz/ZCSIB3AwHgUYcxMBDAX8s4KnYmrISiKbbrbfaxsMeM8">Cooperative Game Theory On-Chain: Part 1</a></b><br></br>
+            <b><a href="https://mode.mirror.xyz/ZCSIB3AwHgUYcxMBDAX8s4KnYmrISiKbbrbfaxsMeM8">Cooperative Game Theory On-Chain: Part 1</a></b><br />
+            <b><a href="https://franklindao.substack.com/p/scroll-opcode-pricing-report">Merkle Trie Space Analysis</a></b><br />
             <b><a href="#/entry/decentralization">The Myth of Decentralization</a></b><br />
             <b>...</b>
         </div>
